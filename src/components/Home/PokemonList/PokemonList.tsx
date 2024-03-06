33f735b4111720fdf930a9e4b0d7cdcc09455c46
idan-gutman <idang@moveohls.com>
2024-03-06 14:02:51 +0200

    finished pokemon list filter

--- src/components/Home/PokemonList/PokemonList.tsx
+++ src/components/Home/PokemonList/PokemonList.tsx
@@ -1,9 +1,18 @@
-import { IPokemonDetails } from "../../../Types";
+import { useState } from "react";
+import { IPokemonDetails, PokemonListResponse } from "../../../Types";
 import { usePokemonList } from "../../../hooks/usePokemons";
 import Pokemon from "../Pokemon/Pokemon";
 
 const PokemonList = () => {
-  const { data: pokemonListData, isLoading: isPokemonListLoading } = usePokemonList();
+  const { data: pokemonListData, isLoading: isPokemonListLoading } =
+    usePokemonList();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredPokemonList =
+    pokemonListData &&
+    pokemonListData?.filter((pokemon: PokemonListResponse) =>
+      pokemon.name.includes(searchQuery)
+    );
 
   if (isPokemonListLoading) {
     return <div>Loading...</div>;
@@ -11,9 +20,18 @@ const PokemonList = () => {
 
   return (
     <div>
-      {pokemonListData.map((pokemon: IPokemonDetails) => (
-        <Pokemon key={pokemon.url} name={pokemon.name} />
-      ))}
+      <div>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search by name or number..."
+        />
+      </div>
+      {!isPokemonListLoading &&
+        filteredPokemonList.map((pokemon: IPokemonDetails) => (
+          <Pokemon key={pokemon.url} name={pokemon.name} />
+        ))}
     </div>
   );
 };
